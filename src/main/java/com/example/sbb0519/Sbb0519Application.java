4e0b2f30f82b849b9c94b2d3a6d9be754e622c56
Jserin <jsr3643@naver.com>
2023-05-19 13:47:13 +0900

    repository0519

---
+++ src/main/java/com/example/sbb0519/Sbb0519Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb0519;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb0519Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb0519Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbb0519/Sbb0519ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb0519;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb0519ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
